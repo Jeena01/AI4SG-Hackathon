c976f0d97dfe1e3e66c7a717cdd31417b2b7c5a1
Yosamin-Esanullah <97552807+Yosamin-Esanullah@users.noreply.github.com>
2024-09-22 10:06:52 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import {useState} from "react";
+
+import "./App.css";
+import { Modal } from "./components/Modal";
+import { Table } from "./components/Table"; 
+
+function App () { 
+
+
+  return (
+     <div className="App">
+    <Table />
+    <Modal />
+    </div>
+  );
+  
+}
+export default App; 
